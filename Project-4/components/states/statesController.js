e55ef829053dc7ebb2c1e833c664943db1b5190e
ultrons <singh.vaibhav.83@gmail.com>
2018-05-01 18:38:21 -0700

    initial version

---
+++ Project-4/components/states/statesController.js
@@ -0,0 +1,33 @@
+/**
+ * Define StatesController for the states component of CS142 project #4
+ * problem #2.  The model data for this view (the states) is available
+ * at window.cs142models.statesModel().
+ */
+
+cs142App.controller('StatesController', ['$scope', function($scope) {
+
+  var allStates = window.cs142models.statesModel();
+
+  if ($scope.search === undefined) {
+    $scope.search={};
+  }
+
+  $scope.search.filterStates = function() {
+    var flist = [];
+    var re = new RegExp($scope.search.str, "i");
+    for (var i in allStates) {
+      if (allStates[i].match(re)) {
+        flist.push(allStates[i]);
+      }
+    }
+    return flist;
+  };
+
+
+
+
+
+   // Replace this with the code for CS142 Project #4, Problem #2
+   console.log('window.cs142models.statesModel()', window.cs142models.statesModel());
+
+}]);
